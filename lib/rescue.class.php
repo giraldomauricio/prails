904eb0feb673e8d93f3e381a457e9161bc9b04cb
Mauricio Giraldo Mutis <mgiraldo@gmail.com>
2013-12-13 11:11:32 -0500

    Updated rescue messages

--- lib/rescue.class.php
+++ lib/rescue.class.php
@@ -17,11 +17,15 @@ class rescue {
     print "The host has no configuration assigned. Please go to the 'hosts' folder and setup a configuration for: ".$_SERVER["HTTP_HOST"];
   }
   
-  public static function NoActionInController($action, $controller)
+  public static function NoActionInController($action = "nil", $controller = "nil")
   {
     print "<div style=\"color:red\"><strong>Prails Error</strong></div><hr />The action [<i>".$action."</i>] is not yet configured in your controller [<i>".$controller."</i>].";
   }
   
+  public static function NoDefaultActionAndController()
+  {
+    print "<div style=\"color:red\"><strong>Prails Error</strong></div><hr />The application has no default Action and Controller. Set them in the config file</i>].";
+  }
 }
 
 ?>
