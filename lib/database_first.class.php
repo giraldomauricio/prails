7359c6cb6331dcba865c7cd13dcca2f1607e10a6
Mauricio Giraldo <mgiraldo@gmail.com>
2014-01-20 10:27:58 -0500

    Fixed backend class name. Added Model creation script.

--- lib/database_first.class.php
+++ lib/database_first.class.php
@@ -21,10 +21,29 @@ class DatabaseFirst extends context {
       $this->_fields = array();
       $sql = "SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='".DBNAME."' AND `TABLE_NAME`='".$table."';";
       $this->ExecuteQuery($sql);
+      $vars = "";
       while ($row = $this->GetRecordObject()) {
-        if($row->COLUMN_KEY == "PRI") $this->_key = $row->COLUMN_NAME;
-        $this->_fields[$row->COLUMN_NAME] = $row->COLUMN_TYPE;
+        if($row->COLUMN_KEY == "PRI")
+        {
+            $this->_key = $row->COLUMN_NAME;
+        }
+        else
+        {
+            $this->_fields[$row->COLUMN_NAME] = $row->COLUMN_TYPE;
+            $vars .= "\t var $".$row->COLUMN_NAME.";\n";
+        }
       }
+      
+      $class =  "";
+      $class .= "<"."?\n";
+      $class .= "class ".$table."_model extends prails {\n";
+      $class .=  "\n";
+      $class .= $vars;
+      $class .=  "\n";
+      $class .=  "\t var $"."_table = '".$table."';\n";
+      $class .=  "\t var $"."_key = '".$this->_key."';\n";
+      $class .= "}\n";
+      $class .= "?".">";
       
     }
   }
