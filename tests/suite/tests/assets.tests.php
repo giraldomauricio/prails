5704fab230929e532cbc719da4cdb0afc7767188
Mauricio Giraldo Mutis <mgiraldo@gmail.com>
2014-01-22 22:28:34 -0500

    Renamed jquery for priority loading Updated test

--- tests/suite/tests/assets.tests.php
+++ tests/suite/tests/assets.tests.php
@@ -2,4 +2,4 @@
 $test->GroupTests("Prails Assets");
 $assets_test = new prails();
 $assets_test->GetAssets();
-$test->AssertTrue(array_key_exists("jquery.js", $assets_test->_assets));
+$test->AssertTrue(array_key_exists("_jquery.js", $assets_test->_assets));
